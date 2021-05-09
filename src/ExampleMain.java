2d31128cf04a8e25458b65e52437bd381a31d28e
Jason Lines <j.lines@uea.ac.uk>
2021-05-09 13:03:55 +0100

    Adding a new print statement to the ExampleMain main method

--- src/ExampleMain.java
+++ src/ExampleMain.java
@@ -2,6 +2,7 @@ public class ExampleMain {
 
     public static void main(String[] args) {
         System.out.println("Hello, I am on GitHub!");
+        System.out.println("Hello, I am also on GitHub");
     }
 
 }
